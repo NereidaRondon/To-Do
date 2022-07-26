501bdc43009695935d340f157f28ca40e54f64e4
Nereida Rondon <Nereida.Rondon5@gmail.com>
2022-07-26 16:06:45 -0400

    To do app done

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-let bgBluePink=()=>{ 
-    document.getElementById('bg').style.backgroundImage ="url('https://images.unsplash.com/photo-1564951434112-64d74cc2a2d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')";
+// console.log(localStorage);
+// localStorage.clear();
+// console.log(localStorage);
+
+let bgGray=()=>{ 
+    document.getElementById('bg').style.backgroundImage ="";
+};
+
+let bgLight=()=>{ 
+    document.getElementById('bg').style.backgroundImage ="url(images/bluemountains.png)";
+};
+
+let bgMarble=()=>{ 
+    document.getElementById('bg').style.backgroundImage ="url(images/pinkbluemarble.png)";
 };
 
 let bgRainbow=()=>{ 
@@ -10,52 +22,61 @@ let bgSmoke=()=>{
     document.getElementById('bg').style.backgroundImage ="url('https://images.unsplash.com/photo-1598760122223-45f0f18a1bbd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')";
     
 };
-
+let myTasks = [];
 
 let id = 0;
 
-
-document.getElementById('add').addEventListener('click', ()=>{
-    let createdDate = new Date();
+let task=()=>{
+    let date = new Date();
     let table = document.getElementById('list');
     let row = table.insertRow(-1);
 
     row.setAttribute('id', `item-${id}`);
+    
     row.insertCell(0).innerHTML = document.getElementById('new-task').value;
-    row.insertCell(1).innerHTML=`${createdDate.getFullYear()}-${createdDate.getMonth()+1}-${createdDate.getDate()}`;
+    
+    row.insertCell(1).innerHTML=`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
     
     row.insertCell(2).innerHTML=document.getElementById('new-start-date').value;
+    
     row.insertCell(3).innerHTML=document.getElementById('new-end-date').value;
 
     let actions = row.insertCell(4);
-    actions.appendChild(createDeleteButton(id++));
+    actions.appendChild(deleteButton(id++));
+  
+    console.log(`Added a row with id: item-${id}`);
+
+    myTasks.push(table);
     document.getElementById('new-task').value='';
-    console.log(endTable);
-});
+    // console.log(myTasks);
+    
+    // localStorage.setItem("Mytable", myTasks);
+}
+   
+// if (typeof(Storage) !== "undefined") {
+//     localStorage.getItem("Mytable", myTasks);
 
-let endTable =document.querySelector("table").rows.length;
+// } else {
+//     document.getElementById("list").innerHTML = "Your browser does not support Web Storage";
+// }
 
-// let endTable2= document.querySelector.table.tBodies[0].rows.length;
-
-console.log(endTable);
-// console.log(endTable2);
-
-
-function createDeleteButton(id){
+function deleteButton(id){
     let btn=document.createElement('button');
 
-    btn.className='btn btn-primary';
+    btn.className='btn btn-outline-dark';
     btn.id=id;
-    btn.innerHTML = '-';
+    btn.innerHTML = '➖';
    
     btn.onclick = ()=>{
-        console.log(`Deleting row with id: item-${id}`);
+        console.log(`Deleting row with id: item-${id+1}`);
         let elementToDelete = document.getElementById(`item-${id}`);
         elementToDelete.parentNode.removeChild(elementToDelete);
     };
-
-    return btn;
-     
+    return btn;   
 };
 
-console.log(endTable);
+// if (JSON.parse(localStorage.getItem("myTasks"))) {
+//   myTasks = JSON.parse(localStorage.getItem("myTasks"))
+//   showTasks(myTasks);
+// }
+
