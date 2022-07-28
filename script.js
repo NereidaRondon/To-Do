569fd56ed22b9df50c7b26312e78dbf62f5be285
Nereida Rondon <Nereida.Rondon5@gmail.com>
2022-07-27 20:59:47 -0400

    Making changes to turn in with Bootstraps

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// console.log(localStorage);
-// localStorage.clear();
-// console.log(localStorage);
-
-let bgGray=()=>{ 
+let bgNone=()=>{ 
     document.getElementById('bg').style.backgroundImage ="";
 };
 
@@ -32,59 +28,47 @@ userInput.addEventListener("keypress", function(event) {
 });
 
 
-let myTasks = [];
-
 let id = 0;
-
 let task=()=>{
     let date = new Date();
     let table = document.getElementById('list');
     let row = table.insertRow(-1);
 
     row.setAttribute('id', `item-${id}`);
+    row.setAttribute('onmouseover', `mouseOver()`);
     
-    row.insertCell(0).innerHTML = document.getElementById('new-task').value;
     
-    row.insertCell(1).innerHTML=`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
+    row.insertCell(0).innerHTML=`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
     
-    row.insertCell(2).innerHTML=document.getElementById('new-start-date').value;
+    row.insertCell(1).innerHTML=document.getElementById('new-start-date').value;
     
-    row.insertCell(3).innerHTML=document.getElementById('new-end-date').value;
+    row.insertCell(2).innerHTML=document.getElementById('new-end-date').value;
+    
+    row.insertCell(3).innerHTML = document.getElementById('new-task').value;
 
-    let actions = row.insertCell(4);
-    actions.appendChild(deleteButton(id++));
-  
+
+    let deleteAction = row.insertCell(4);
+    deleteAction.appendChild(deleteButton(id++));
+
     console.log(`Added a row with id: item-${id}`);
 
-    myTasks.push(table);
     document.getElementById('new-task').value='';
-    // console.log(myTasks);
+    document.getElementById('new-start-date').value='';
+    document.getElementById('new-end-date').value='';
+    console.log(`Fields are cleared`);   
     
-    // localStorage.setItem("Mytable", myTasks);
 }
-   
-// if (typeof(Storage) !== "undefined") {
-//     localStorage.getItem("Mytable", myTasks);
-
-// } else {
-//     document.getElementById("list").innerHTML = "Your browser does not support Web Storage";
-// }
-
+ 
 function deleteButton(id){
     let btn=document.createElement('button');
     btn.className='btn btn-outline-dark';
     btn.id=id;
-    btn.innerHTML = '➖';
+    btn.innerHTML = '🗑️';
    
     btn.onclick = ()=>{
         console.log(`Deleting row with id: item-${id+1}`);
-        let elementToDelete = document.getElementById(`item-${id}`);
-        elementToDelete.parentNode.removeChild(elementToDelete);
+        let taskToDelete = document.getElementById(`item-${id}`);
+        taskToDelete.parentNode.removeChild(taskToDelete);
     };
     return btn;   
 };
-
-// if (JSON.parse(localStorage.getItem("myTasks"))) {
-//   myTasks = JSON.parse(localStorage.getItem("myTasks"))
-//   showTasks(myTasks);
-// }
